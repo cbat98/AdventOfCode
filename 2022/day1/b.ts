a282e9e60567c8c78ba732350e738860b664b05b
Charlie Batten <charlie.batten@microlise.com>
2023-01-09 14:38:53 +0000

    Node to Deno: Day1

--- 2022/day1/b.ts
+++ 2022/day1/b.ts
@@ -1,17 +1,15 @@
-const fs = require("fs");
+const input = await Deno.readTextFile("input.txt")
 
-fs.readFile("./day1/input.txt", "utf8", (err:string, input:string) => {
-    let elves : any = input.split("\n\n");
+let elves : any = input.split("\n\n");
 
-    elves = elves.map((elf:string) => {
-        return elf.split("\n").map((f:string) => parseInt(f) || 0)
-    })
-
-    let sums = elves.map((e:number[]) => e.reduce((total:number, f:number) => total += f))
-
-    sums.sort((a:number,b:number) => a - b)
-
-    let top3 = sums.slice(sums.length - 3)
-
-    console.log(top3.reduce((total:number, e:number) => total += e))
+elves = elves.map((elf:string) => {
+    return elf.split("\n").map((f:string) => parseInt(f) || 0)
 })
+
+let sums = elves.map((e:number[]) => e.reduce((total:number, f:number) => total += f))
+
+sums.sort((a:number,b:number) => a - b)
+
+let top3 = sums.slice(sums.length - 3)
+
+console.log(top3.reduce((total:number, e:number) => total += e))
