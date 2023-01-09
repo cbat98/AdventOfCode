1fb2aba5afaa5aa4aec799caf08daa7d46257722
Charlie Batten <charlie.batten@microlise.com>
2023-01-09 14:22:20 +0000

    Removed created js files

--- 2022/day1/a.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-fs.readFile("./input.txt", "utf8", function (err, input) {
-    var elves = input.split("\n\n");
-    elves = elves.map(function (elf) {
-        return elf.split("\n").map(function (f) { return parseInt(f) || 0; });
-    });
-    var sums = elves.map(function (e) { return e.reduce(function (total, f) { return total += f; }); });
-    console.log("max", Math.max.apply(Math, sums));
-});
