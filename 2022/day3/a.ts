fff12487977ad6b8012d37719b74b885809d0024
Charlie Batten <charlie.batten@microlise.com>
2023-01-09 14:46:03 +0000

    Node to Deno: Day3

--- 2022/day3/a.ts
+++ 2022/day3/a.ts
@@ -1,24 +1,22 @@
-const fs = require("fs");
-
 const PRIORITIES = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 
-fs.readFile("./day3/input.txt", "utf8", (err:string, input:string) => {
-    let rucksacks : string[] = input.split("\n")
-    rucksacks.pop()
+const input = await Deno.readTextFile("./input.txt")
 
-    const rucksacksComp : string[][] = rucksacks.map(r => {
-        return [r.substring(0, r.length / 2), r.substring(r.length / 2)]
-    })
+let rucksacks : string[] = input.split("\n")
+rucksacks.pop()
 
-    const intersections : string[] = rucksacksComp.map(r => {
-        return r[0].split("").filter(c => r[1].includes(c))[0]
-    })
-
-    const sum = intersections.map(i => {
-        return PRIORITIES.indexOf(i)
-    }).reduce((tot, i) => {
-        return tot += i
-    })
-
-    console.log(sum)
+const rucksacksComp : string[][] = rucksacks.map(r => {
+    return [r.substring(0, r.length / 2), r.substring(r.length / 2)]
 })
+
+const intersections : string[] = rucksacksComp.map(r => {
+    return r[0].split("").filter(c => r[1].includes(c))[0]
+})
+
+const sum = intersections.map(i => {
+    return PRIORITIES.indexOf(i)
+}).reduce((tot, i) => {
+    return tot += i
+})
+
+console.log("total", sum)
--- 2022/day3/b.ts
+++ 2022/day3/b.ts
@@ -1,25 +1,23 @@
-const fs = require("fs");
-
 const PRIORITIES = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 
-fs.readFile("./day3/input.txt", "utf8", (err:string, input:string) => {
-    let rucksacks : string[] = input.split("\n")
-    rucksacks.pop()
+const input = await Deno.readTextFile("./input.txt")
 
-    let rucksacksComp : string[][] = []
-    for(let i = 0; i < rucksacks.length; i += 3) {
-        rucksacksComp.push([rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]]);
-    }
+let rucksacks : string[] = input.split("\n")
+rucksacks.pop()
 
-    const intersections : string[] = rucksacksComp.map(r => {
-        return r[0].split("").filter(c => r[1].includes(c) && r[2].includes(c))[0]
-    })
+let rucksacksComp : string[][] = []
+for(let i = 0; i < rucksacks.length; i += 3) {
+    rucksacksComp.push([rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]]);
+}
 
-    const sum = intersections.map(i => {
-        return PRIORITIES.indexOf(i)
-    }).reduce((tot, i) => {
-        return tot += i
-    })
-
-    console.log(sum)
+const intersections : string[] = rucksacksComp.map(r => {
+    return r[0].split("").filter(c => r[1].includes(c) && r[2].includes(c))[0]
 })
+
+const sum = intersections.map(i => {
+    return PRIORITIES.indexOf(i)
+}).reduce((tot, i) => {
+    return tot += i
+})
+
+console.log("total", sum)
